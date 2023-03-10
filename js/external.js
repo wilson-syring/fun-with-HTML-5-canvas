a1901f76c05a4f47d6618cf8f0c2462fe428ee77
Wilson Syring <wilson.syring@gmail.com>
2023-03-10 13:20:47 -0600

    HTML canvas exercise in progress seems to be working well

--- js/external.js
+++ js/external.js
@@ -4,23 +4,35 @@ const ctx = canvas.getContext('2d');
 
 //settings for the canvas element
 canvas.width = window.innerWidth;
-canvas.hieght = window.innerHeight;
+canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 50;
 
 //variables for the drawing location on canvas
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 //function for drawing
 function draw(e) {
     if (!isDrawing) return;
+    ctx.strokeStyle = `hsl(${hue},100%, 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
 }
 
 //eventlistners for the mouse movement
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown',() => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
